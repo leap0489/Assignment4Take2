6b6e2399778ccd5dac9de7fb2de3628a19395499
leap0489 <leap0489@users.noreply.github.com>
2024-03-28 15:25:49 +0000

    deploy: 0ce07d9a4a88b40ced2338fc4ece365cd68d366c

--- wp-content/themes/assignment-4/customElements.js
+++ wp-content/themes/assignment-4/customElements.js
@@ -5,3 +5,26 @@ class CopyrightYear extends HTMLElement{
 }
 
 customElements.define("x-year", CopyrightYear);
+
+  class DaysLeft extends HTMLElement{
+        connectedCallback(){
+            let today=new Date();
+            let nPiDay=new Date(today.getFullYear(), 4, 1);
+            let nSeconds = 1000*60*60*24;
+            let nDaysLeft = Math.ceil((nPiDay.getTime()-today.getTime())/(nSeconds));
+            console.log(nDaysLeft);
+			this.innerHTML = nDaysLeft
+        }
+    }
+
+    customElements.define("x-daysleft", DaysLeft);
+
+    class SaleDays extends HTMLElement{
+        connectedCallback(){
+            let today=new Date();
+            let nPiDay=new Date(today.getFullYear(), 4, 1);
+            this.innerHTML = nPiDay.toLocaleDateString('en-CA');
+        }
+    }
+
+    customElements.define("x-saledays", SaleDays);
